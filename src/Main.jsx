d213022f1dc91c9a428798d7e999d5471d0667ab
Charles S <charlessoaresdossantos@gmail.com>
2019-03-09 10:57:06 -0300

    removed unused files and library

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './main.css';
-import NoImage from './no-img-landscape-text.png';
-import axios from 'axios';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import {getShow} from './actions/showActions';
+import { getShow } from './actions/showActions';
 
 class Main extends Component {
     constructor(props) {
