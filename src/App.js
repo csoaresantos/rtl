68242a1afb03a80f5b6fa98d968d84d5c11e7c65
Charles S <charlessoaresdossantos@gmail.com>
2019-03-09 11:17:10 -0300

    refactoring the app component to get more concise

--- src/App.js
+++ src/App.js
@@ -1,76 +1,19 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './main.css';
-import NoImage from './no-img-landscape-text.png';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import {getEpisodes} from './actions/episodesActions';
 import Main from './Main';
+import EpisodeList from './EpisodeList';
 
-class App extends Component {
-
+export default class App extends Component {
   constructor(props) {
     super(props);
   }
-  
-  componentWillMount() {
-    this.props.getEpisodes();
-  }
 
-  render() {
-    
-    const episodes = this.props.list ? this.props.list : [];
-    
+  render() {    
     return (
       <main role="main">
-  
         <Main />
-
-
-      
-        <div class="album py-5 bg-light">
-          <div class="container">
-          <h2>Episodes</h2>
-            <div class="row">
-              {
-                episodes.map(data =>  {
-
-                let episodeSummary = data.summary;
-                episodeSummary = episodeSummary != undefined ? episodeSummary.replace(/<p>/g, '') : null;
-                episodeSummary = episodeSummary != undefined ? episodeSummary.replace('</p>', '') : null;
-
-                  const link = `/EpisodeDetail/${data.id}`;
-                  return (
-                    <div class="col-md-4" key={data.id}>
-                      <div class="card mb-4 shadow-sm">
-                    
-                        <img src={data.image != undefined ? data.image.medium : NoImage} />
-                        <div class="card-body">
-                          <strong class="d-inline-block mb-2 text-primary">{data.name}</strong>
-                          <p class="card-text">{episodeSummary}</p>
-                          <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                              <Link to={link}>View Episode</Link>
-                            </div>
-                            <small class="text-muted">{data.runtime}</small>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  )
-                }
-              )
-            }
-
-          </div>
-        </div>
-      </div>
+        <EpisodeList />
       </main>
     );
   }
 }
-
-const mapStateToProps = state => ({ list: state.list.episode })
-const mapDispatchToProps = dispatch => bindActionCreators({getEpisodes}, dispatch);
-export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/EpisodeDetail.jsx
+++ src/EpisodeDetail.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import NoImage from './no-img-landscape-text.png';
 import { connect } from 'react-redux';
