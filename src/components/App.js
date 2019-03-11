1f38aded09fddc4ece042d67ea1ac2028943621a
Charles S <charlessoaresdossantos@gmail.com>
2019-03-10 21:44:34 -0300

    refactoring to turn statefull component into stateless

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,9 @@ import './main.css';
 import HeaderShow from './HeaderShow';
 import EpisodeList from './EpisodeList';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {    
-    return (
-      <main role="main">
-        <HeaderShow />
-        <EpisodeList />
-      </main>
-    );
-  }
-}
+export default () => (
+    <main role="main">
+      <HeaderShow />
+      <EpisodeList />
+    </main>
+)
